e160c46351f54f9db0edfa5e7a30cdc2b4bdb980
rachita-devakumar <rachita-devakumar@gmail.com>
2015-07-12 04:06:48 +0530

    php files

--- survey.php
+++ survey.php
@@ -1,9 +1,9 @@
 <?php
+echo "at the start";
 include("config.inc.php");
 $Dbcon = new connection();
 $Dbcon->localarea();
-?>
-<?php
+
 
 $schoolname = $_POST["school"];
 $schoolauth = $_POST["schoolauth"];
@@ -16,18 +16,29 @@ $amount = $_POST["amt"];
 $constcost = $_POST["cost"];
 $newresource = $_POST["newreso"];
 $prone = $_POST["prone"];
+$value=($disasteraffected*5)+(($numberofstudents/$studentpop)*4)+($dest*3)+($newresource*2)+($prone);
+echo "Value is".$value;
 
 if(isset($_POST["submit"]))
 {
-
-	echo "Hello";
-	$SqlContent1 = $Dbcon->ExecuteQuery("insert into survey values('','$schoolname','$schoolauth','$disasteraffected','$numberofstudents','$studentpop','$dest','$comm','$constcost','$newresource','$prone','$amount')");
+	 echo "inside submit";
+	$SqlContent1 = $Dbcon->ExecuteQuery("insert into survey values('','schoolname','$schoolauth','$disasteraffected','$numberofstudents',
+		'$studentpop','$dest','$comm','$constcost','$newresource','$prone','$amount','$value')");
 
 	if($SqlContent1)
 	{
-	 echo "Done!";
+	 echo "Your response has been recorded.";
+	 mail("rachitadevakumar@gmail.com","My subject","Your response");
+	 echo $value;
 	}
 	else
-		echo "Not done";
+		echo "Sorryskvbiu!";
+
 }
+else
+{
+	echo "No submit";
+}
+
+
 ?>
