fc95183ee80ebc17a2029bf40999027661b4922a
Aanchal94 <aanchal.dadlani@gmail.com>
2015-07-12 11:39:52 +0530

    Graphs

--- yes/report.php
+++ yes/report.php
@@ -12,7 +12,42 @@ $Dbcon->localarea();
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
 
+      // Load the Visualization API and the piechart package.
+      google.load('visualization', '1.0', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'zz');
+        data.addColumn('number', 'zz');
+        data.addRows([
+          ['Singhania', 3],
+          ['Fatima', 1],
+          ['Don bosco', 1],
+          ['NCT', 1],
+          ['Jai hind', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'Priority analysis of school',
+                       'width':800,
+                       'height':500};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
 <body background="bg.jpg">
 
 <div class="container">
@@ -57,6 +92,6 @@ $Dbcon->localarea();
   </table>
   </div>
 </div>
-
+<center><div id="chart_div"></div></center>
 </body>
 </html>
